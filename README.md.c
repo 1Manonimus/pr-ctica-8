a42479ecdcca3565b504a357b804e34c75c8a658
Elver Galarga <mangoancestral@gmail.com>
2025-04-03 14:43:52 -0600

    Create README.md.c

---
+++ README.md.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+printf("introduzca un número, se obtendra el número de pares he impares contendidos en el, además se mostrara cual es último par y el ultimo impar\n");
+scanf("%d",&n);
+do
+{
+sum=1+cont;
+cont=sum;
+    if(sum%2==0)
+    {
+        npar=1+cont2;
+        cont2=npar;
+        parf=sum;
+    }
+    else
+    {
+        nimpar=1+cont3;
+        cont3=nimpar;
+        imparf=sum;
+    }
+}
+while(sum<=n);
+printf("EL número de pares dentro de tu número es;%d\n",)
+
+    return 0;
+}
