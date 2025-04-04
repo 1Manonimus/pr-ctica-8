09c11d1d935dbb53c15d04da2a9ec0e8f9144abd
Elver Galarga <mangoancestral@gmail.com>
2025-04-03 22:04:48 -0600

    Create ejercicio 1.c

---
+++ ejercicio 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int i=1;
+    printf("los primeros 100 números enteros son;\n");
+
+while(i<=100)
+{
+    printf("%d\n",i);
+    i=i+1;
+}
+    return 0;
+}
